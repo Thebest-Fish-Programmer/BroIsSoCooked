cf71aca0d544139d20c3a144fdc656c7652021f1
Thebest-Fish-Programmer <engrdkg@gmail.com>
2025-08-22 20:36:53 -0600

    Add files via upload

---
+++ Assets/Static/script.js
@@ -0,0 +1,16 @@
+const images = document.querySelectorAll('.moving-image');
+
+function moveImages() {
+    const maxY = 3000;
+    const maxX = window.innerWidth - 950;
+
+    images.forEach(img => {
+        const x = Math.random() * maxX;
+        const y = Math.random() * maxY;
+        img.style.transform = `translate(${x}px, ${y}px)`;
+    });
+}
+
+setInterval(moveImages, 3000);
+moveImages();
+
