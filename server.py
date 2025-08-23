6954e07a171c3b45a5272a1b97ffb3ecee9d2861
Thebest-Fish-Programmer <engrdkg@gmail.com>
2025-08-22 20:57:14 -0600

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_folder="Assets", template_folder=".")
 
 @app.route("/")
 def index():
-    return send_from_directory(".", "index.html")
+    return send_from_directory(".", "LaunchWeb.html")
 
 @app.route("/Assets/<path:filename>")
 def assets(filename):
